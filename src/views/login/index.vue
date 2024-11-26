697fbe6702d028510f14068e1a691b713ac2fbc9
Rockysun <869385454@qq.com>
2024-11-27 01:03:22 +0800

    jhgj

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,8 +2,8 @@
     <h1>登录</h1>
 </template>
 <script setup lang="ts">
-import { useUserStore } from '@/store/user';
-const userStore=useUserStore();
+// import { useUserStore } from '@/store/user';
+// const userStore=useUserStore();
 console.log('oookß')
 
 </script>
